97dcd7902218075aec40766ae2d1ae91194460d2
4acha4a <danik.mad@gmail.com>
2024-09-16 12:27:18 +0300

    Update test_hito.py

--- src/test_hito.py
+++ src/test_hito.py
@@ -7,22 +7,21 @@ ser = serial.Serial(
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE
-) # 3D-принтер, на который будет посылаться Gcode
-# Необходимо задать значения отклонения по координатам для задания нулевого положения 3D-принтера на ЧПУ
+)
 offset_x = 33 + 23.35
 offset_y = 28.6
 const_offset_screw = 5.5 
 
 if __name__ == "__main__":
     file_path = './test_hito/test_query.txt'
-    file = open(file_path).read() # Считывание комманд с файла
+    file = open(file_path).read()
     os.chdir('./test_hito')
     params = Parameters(offset_x, offset_y, const_offset_screw)
     cmd_parser = CommandParser(params=params, ser=ser) 
     lines = file.split('\n')
     cmd_parser.gcode_to_device("G28")
     for line in lines:
-        result = cmd_parser.parse_command(line) # Обработка комманд с файла
+        result = cmd_parser.parse_command(line)
         if result == False:
             print("Aborting")
             break
